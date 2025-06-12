2125722d7152f6bf7e12ba5bbe9059f7b44365ae
Ya1chkosoft <Ya1chkosoft@gmail.com>
2025-06-12 20:37:39 +0300

    исправление ошибки путей

--- start_bot.sh
+++ start_bot.sh
@@ -2,10 +2,9 @@
 # start_bot.sh — должен лежать в /root/RPBOT-MAIN
 
 # 1) Переходим в каталог с ботом
-cd "/root/RPBOT-MAIN/Python files" || exit 1
+cd "/root/RPBOT-MAIN/Python files"
 
-# 2) Активируем виртуальное окружение
-source .venv/bin/activate
+source ../.venv/bin/activate 
 
 # 3) Запускаем бота (замена exec — чтобы система получала статус процесса бота)
 exec python RPBot3.0.py

5321763cadc05a3887ca577433eeef6d6bc6f525
Ya1chkosoft <Ya1chkosoft@gmail.com>
2025-06-12 15:53:41 +0300

    обновил код, добавил rpbot.sh и сервис

--- RP BOT 3.5/RPBOT/Python files/app/database/init_db_runner.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from app.database.models import async_main
-
-if __name__ == "__main__":
-    # запускаем асинхронное создание таблиц
-    asyncio.run(async_main())
-    print(" Таблицы созданы.")

627ac95b3600bd5538f77dfa3857a5e3f7709138
Ya1chko <ya1chkosoft@gmail.com>
2025-08-30 17:25:34 +0300

    3.2

--- start_bot.sh
+++ start_bot.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# start_bot.sh — должен лежать в /root/RPBOT-MAIN
 
 # 1) Переходим в каталог с ботом
-cd "/root/RPBOT-MAIN/python-files"
+cd "python-files"
 
-source ../.venv/bin/activate 
+# 2) Активируем виртуальную среду
+source .venv/bin/activate
 
-# 3) Запускаем бота (замена exec — чтобы система получала статус процесса бота)
+# 3) Запускаем бота
 exec python RPBot3.0.py
